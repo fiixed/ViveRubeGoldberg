c84ce145084e6d4269e51664c5402e997438178e
Andrew Bell <fiixed@gmail.com>
2017-05-01 20:17:43 -0500

    feat:  add controller menu

--- Assets/Scripts/ControllerInputManager.cs
+++ Assets/Scripts/ControllerInputManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ControllerInputManager: MonoBehaviour {
     public SteamVR_TrackedObject trackedObj;
     public SteamVR_Controller.Device device;
+    public ObjectMenuManager objectMenuManager;
 
     // Teleporter
     private LineRenderer laser;
@@ -17,6 +18,14 @@ public class ControllerInputManager: MonoBehaviour {
     // Grab & Throw
     private float throwforce = 1.5f;
 
+    // Swipe
+    public float swipeSum;
+    public float touchLast;
+    public float touchCurrent;
+    public float distance;
+    public bool hasSwipedLeft;
+    public bool hasSwipedRight;
+
     // Use this for initialization
     void Start () {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
@@ -26,6 +35,45 @@ public class ControllerInputManager: MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device == SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost))) {
+            if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+                touchLast = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+            }
+            if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad)) {
+                touchCurrent = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+                distance = touchCurrent - touchLast;
+                touchLast = touchCurrent;
+                swipeSum += distance;
+                if (!hasSwipedRight) {
+                    if (swipeSum > 0.5f) {
+                        swipeSum = 0;
+                        SwipeRight();
+                        hasSwipedRight = true;
+                        hasSwipedLeft = false;
+                    }
+                }
+                if (!hasSwipedLeft) {
+                    if (swipeSum < -0.5f) {
+                        swipeSum = 0;
+                        SwipeLeft();
+                        hasSwipedRight = false;
+                        hasSwipedLeft = true;
+                    }
+                }
+                
+            }
+            if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad)) {
+                swipeSum = 0;
+                touchCurrent = 0;
+                touchLast = 0;
+                hasSwipedRight = false;
+                hasSwipedLeft = false;
+            }
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+                SpawnObject();
+            }
+        }
         if (device == SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost))) {
             if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad)) {
                 laser.gameObject.SetActive(true);
@@ -80,6 +128,18 @@ public class ControllerInputManager: MonoBehaviour {
         rb.isKinematic = false;
         rb.velocity = device.velocity * throwforce;
         rb.angularVelocity = device.angularVelocity;
+    }
+
+    private void SwipeLeft() {
+        objectMenuManager.MenuLeft();
+    }
+
+    private void SwipeRight() {
+        objectMenuManager.MenuRight();
+    }
+
+    private void SpawnObject() {
+        objectMenuManager.SpawnCurrenObject();
     }
 
 }
