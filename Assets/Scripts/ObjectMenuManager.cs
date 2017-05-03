e72cb52b2b12a556da32d5bda88e0833f3f8e342
Andrew Bell <fiixed@gmail.com>
2017-05-02 21:30:12 -0500

    feat:  finish controller menu and rube object instantiate

--- Assets/Scripts/ObjectMenuManager.cs
+++ Assets/Scripts/ObjectMenuManager.cs
@@ -5,31 +5,32 @@ using UnityEngine;
 public class ObjectMenuManager : MonoBehaviour {
     public List<GameObject> objectList;
     public List<GameObject> objectPrefabList;
+    public List<GameObject> objectMenuUI;
     public int currentObject = 0;
 
 	// Use this for initialization
 	void Start () {
-        foreach (Transform child in transform) {
-            objectList.Add(child.gameObject);
-        }
+        //foreach (Transform child in transform) {
+        //    objectList.Add(child.gameObject);
+        //}
 	}
 	
 	public void MenuLeft() {
-        objectList[currentObject].SetActive(false);
+        objectMenuUI[currentObject].SetActive(false);
         currentObject--;
         if (currentObject < 0) {
             currentObject = objectList.Count - 1;
         }
-        objectList[currentObject].SetActive(true);
+        objectMenuUI[currentObject].SetActive(true);
     }
 
     public void MenuRight() {
-        objectList[currentObject].SetActive(false);
+        objectMenuUI[currentObject].SetActive(false);
         currentObject++;
         if (currentObject > objectList.Count - 1) {
             currentObject = 0;
         }
-        objectList[currentObject].SetActive(true);
+        objectMenuUI[currentObject].SetActive(true);
     }
 
     public void SpawnCurrenObject() {
